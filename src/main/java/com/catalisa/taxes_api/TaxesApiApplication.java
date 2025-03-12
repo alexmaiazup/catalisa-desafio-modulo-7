b25cb0cd825be2164e84b632aedf0be336cc7c86
Alex Zup <alex.cunha@zup.com.br>
2025-03-12 11:30:29 -0300

    initial setup

---
+++ src/main/java/com/catalisa/taxes_api/TaxesApiApplication.java
@@ -0,0 +1,14 @@
+package com.catalisa.taxes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxesApiApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
