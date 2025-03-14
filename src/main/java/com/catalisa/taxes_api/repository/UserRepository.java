a79e8c951fc07cfa49ad728950aa3bd6e2e80d4d
Alex Zup <alex.cunha@zup.com.br>
2025-03-14 20:22:31 -0300

    refactor: changes import to reflect new project structure

--- src/main/java/com/catalisa/taxes_api/repository/UserRepository.java
+++ src/main/java/com/catalisa/taxes_api/repository/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.catalisa.taxes_api.model.entity.User;
+import com.catalisa.taxes_api.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
