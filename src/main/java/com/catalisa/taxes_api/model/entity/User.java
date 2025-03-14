962f3977bf0ca1d3a44e7dd1c3ed452e29cb547b
Alex Zup <alex.cunha@zup.com.br>
2025-03-14 14:34:11 -0300

    feat: implements both user and role entities

---
+++ src/main/java/com/catalisa/taxes_api/model/entity/User.java
@@ -0,0 +1,40 @@
+package com.catalisa.taxes_api.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Role role;
+}
---
+++ src/main/java/com/catalisa/taxes_api/model/enums/RoleUser.java
@@ -0,0 +1,6 @@
+package com.catalisa.taxes_api.model.enums;
+
+public enum RoleUser {
+    ROLE_DEFAULT,
+    ROLE_ADMIN,
+}
