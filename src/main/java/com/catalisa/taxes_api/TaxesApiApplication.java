c91fd7ab65017b6d9f947fcc68f851c2308fe2b3
Alex Zup <alex.cunha@zup.com.br>
2025-03-24 11:59:15 -0300

    feats: adds dotenv lib

--- src/main/java/com/catalisa/taxes_api/TaxesApiApplication.java
+++ src/main/java/com/catalisa/taxes_api/TaxesApiApplication.java
@@ -3,10 +3,13 @@ package com.catalisa.taxes_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class TaxesApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(TaxesApiApplication.class, args);
 	}
 
